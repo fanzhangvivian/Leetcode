5381d3f9be41355a6b316bb1b9eb545d70709d5c
fanzhangvivian <fanzhangvivian@outlook.com>
2024-01-10 23:37:44 -0800

    Create 27-remove-element-easy.py

---
+++ array/27-remove-element-easy.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=27 lang=python
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        for x in nums:
+            if x != val:
+                nums[i] = x
+                i += 1
+        return i
+        
+# @lc code=end
+
