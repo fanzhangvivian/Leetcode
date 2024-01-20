b1850b53f825969e23a3e5179e913f7511d8fada
fanzhangvivian <fanzhangvivian@outlook.com>
2024-01-19 22:35:01 -0800

    Leetcode 20240119 Tree__Q222__Review

--- tree/222-count-complete-tree-nodes-easy.py
+++ tree/222-count-complete-tree-nodes-easy.py
@@ -19,46 +19,52 @@ class Solution(object):
         :rtype: int
         """
         #递归法
-        # if not root:
-        #     return 0
-        # left_count = self.countNodes(root.left)
-        # right_count = self.countNodes(root.right)
-        # return 1 +left_count + right_count
-
-        #层序遍历————迭代法
-        # if not root:
-        #     return 0
-        
-        # queue = collections.deque([root])
-        # count = 0
-        # while queue:
-        #     len_queue = len(queue)
-        #     count += len_queue
-
-        #     for i in range(len_queue):
-        #         node = queue.popleft()
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        
-        # return count
-
+        #利用的是普通二叉树递归
         if not root:
             return 0
-        left_depth = 0
-        right_depth = 0
+        left_count = self.countNodes(root.left)
+        right_count = self.countNodes(root.right)
+        return 1 +left_count + right_count
+
+        #层序遍历————迭代法
+        #利用的是普通二叉树层序遍历
+        if not root:
+            return 0
+        
+        queue = collections.deque([root])
+        count = 0
+        while queue:
+            len_queue = len(queue)
+            count += len_queue
+
+            for i in range(len_queue):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return count
+
+
+        #利用完全二叉树属性进行递归
+        #如果是满二叉树，节点数则为2**深度 - 1，要先判断深度是否一致，如果一致则直接返回
+        if not root:
+            return 0
+        left_depth = 1
+        right_depth = 1
         left = root.left
         right = root.right
         while left:
             left = left.left
             left_depth += 1
         while right:
-            right = root.right
+            right = right.right
             right_depth += 1
 
-        if right_depth == left_depth:
+        if left_depth == right_depth :
             return 2**left_depth - 1
+        
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
      
                
